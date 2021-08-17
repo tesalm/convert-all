987e5df2e4aa1fb4c26bcc7f44b93e5175679768
Teemu Salminen <salminen.tm@hotmail.com>
2021-08-18 00:16:21 +0300

    component files renaming

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StatusBar, StatusBarIOS } from 'react-native';
+import { StatusBar } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
 import QuantitySelectionView from './src/screens/QuantitySelection';
